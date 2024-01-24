319623eca5a96ad60d62bad67c558fa36708bdad
Alonso-GarnierBBDO <alonso.artavia@garnierbbdo.com>
2024-01-24 09:48:22 -0600

    Se agrego el 404

---
+++ error.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <AppHeader/>
+        <section>
+            <div class="page container_404">
+                <div>
+                    <h2>404</h2>
+                    <p>Pagina no encontrada</p>
+                    <NuxtLink to="/">Volver al inicio</NuxtLink>
+                </div>
+            </div>
+        </section>
+        <AppFooter/>
+    </div>
+</template>
